01831cdfdc56c7601a6d05a3e08f564761f8662b
pongpatapee <pongpatapee1021@gmail.com>
2023-05-31 11:12:24 -0700

    my attempt to get lsp/formatting to work

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,5 +1,22 @@
 local plugins = {
 
+  -- lsp config for linting and formatting
+  {
+    "neovim/nvim-lspconfig",
+
+    dependencies = {
+      "jose-elias-alvarez/null-ls.nvim",
+      config = function()
+        require "custom.configs.null-ls"
+      end,
+    },
+
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+
   -- for seemless navigation with tmux panes
   {
     "christoomey/vim-tmux-navigator",
