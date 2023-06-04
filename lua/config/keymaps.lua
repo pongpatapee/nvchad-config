c565a91e40265cbf4cd1843f64b1d0588b13c856
pongpatapee <pongpatapee1021@gmail.com>
2023-06-04 00:42:20 -0700

    added qof keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -23,3 +23,15 @@ vim.keymap.set("v", "<C-_>", "<ESC><cmd>lua require('Comment.api').toggle.linewi
 -- split screen
 map("n", "<leader>\\", "<cmd> vsplit<CR>", { desc = "Split window right", noremap = true, silent = true }) -- alternate bind to <leader> |
 map("n", "<leader>_", "<cmd> split<CR>", { desc = "Split window below", noremap = true, silent = true }) -- alternate bind to <leader> -
+
+-- center page when C-d/C-u
+map("n", "<C-u>", "<C-u>zz", opts)
+map("n", "<C-d>", "<C-d>zz", opts)
+
+-- keeps cursor where it is after yank (by default it goes back up to the top)
+map("n", "y", "ygv<ESC>", opts)
+
+-- allow for Ctrl + Backspace for deleting words
+-- not working right now for some reason
+-- map("i", "<C-BS>", "<ESC>cvb", {})
+map("i", "<C-BS>", "<C-w>", opts)
