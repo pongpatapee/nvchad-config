4dc9c75c8df32ae0daeaf4079a78a7e8b2ba7c25
pongpatapee <pongpatapee1021@gmail.com>
2023-06-04 23:24:11 -0700

    added colorizer

--- lua/plugins/general.lua
+++ lua/plugins/general.lua
@@ -6,6 +6,7 @@ return {
     lazy = false,
   },
 
+  -- added commenting
   {
     "numToStr/Comment.nvim",
     keys = {
@@ -14,5 +15,29 @@ return {
       { "gbc", mode = "n" },
       { "gb", mode = "v" },
     },
+  },
+
+  --colorizer
+  {
+    "norcalli/nvim-colorizer.lua",
+
+    config = function()
+      -- require("colorizer").setup({
+      --   filetypes = { "*" },
+      --   user_default_options = {
+      --     RGB = true, -- #RGB hex codes
+      --     RRGGBB = true, -- #RRGGBB hex codes
+      --     names = true, -- "Name" codes like Blue
+      --     RRGGBBAA = false, -- #RRGGBBAA hex codes
+      --     rgb_fn = true, -- CSS rgb() and rgba() functions
+      --     hsl_fn = false, -- CSS hsl() and hsla() functions
+      --     css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+      --     css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+      --     -- Available modes: foreground, background
+      --     mode = "background", -- Set the display mode.
+      --   },
+      -- })
+      require("colorizer").setup()
+    end,
   },
 }
