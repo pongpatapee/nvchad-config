31c78bc7e6bacd5090b7ca526adbe6a7545acf07
pongpatapee <pongpatapee1021@gmail.com>
2023-06-05 12:05:07 -0700

    tried getting prettier formatter to work

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -10,6 +10,4 @@ return {
       },
     },
   },
-
-  { import = "lazyvim.plugins.extras.formatting.prettier" },
 }
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,14 +1,18 @@
 return {
   {
     "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "stylua",
-        "shellcheck",
-        "shfmt",
-        "pyright",
-        "deno",
-      },
-    },
+    opts = function(_, opts)
+      table.insert(opts.ensure_installed, "prettierd")
+      table.insert(opts.ensure_installed, "stylua")
+      table.insert(opts.ensure_installed, "shellcheck")
+      table.insert(opts.ensure_installed, "shfmt")
+      table.insert(opts.ensure_installed, "pyright")
+      table.insert(opts.ensure_installed, "deno")
+    end,
+
+    -- opts = {
+    --   ensure_installed = {
+    --   },
+    -- },
   },
 }
