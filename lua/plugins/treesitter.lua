0ef93293660b8a2fbb3a4d999ff0255249f6130e
pongpatapee <pongpatapee1021@gmail.com>
2023-06-13 16:30:12 -0700

    added rainbow brackets

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,9 @@
 return {
   {
+    "HiPhish/nvim-ts-rainbow2",
+  },
+
+  {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
@@ -17,6 +21,13 @@ return {
         "typescript",
         "vim",
         "yaml",
+      },
+
+      rainbow = {
+        enable = true,
+        -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+        extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+        -- max_file_lines = nil, -- Do not enable for files with more than n lines, int
       },
     },
   },
