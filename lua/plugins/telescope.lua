7d2fe30bddd582b7729d9dc5a6ff18f417f27010
pongpatapee <pongpatapee1021@gmail.com>
2023-06-21 17:14:36 -0700

    telescope media file now works! (need chafa version with animate)

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,16 +7,27 @@ return {
       config = function()
         require("telescope").load_extension("media_files")
       end,
-      opts = {
-        extensions = {
-          media_files = {
-            -- filetypes whitelist
-            -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
-            filetypes = { "png", "webp", "jpg", "jpeg" },
-            -- find command (defaults to `fd`)
-            find_cmd = "rg",
-          },
+    },
+
+    require("telescope").setup({
+      extensions = {
+        media_files = {
+          -- filetypes whitelist
+          -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
+          filetypes = { "png", "webp", "jpg", "jpeg" },
+          -- find command (defaults to `fd`)
+          find_cmd = "fdfind",
         },
+      },
+    }),
+
+    keys = {
+      {
+        "<leader>fi", -- Find images/media files
+        function()
+          require("telescope").extensions.media_files.media_files()
+        end,
+        desc = "Find Images/Media Files",
       },
     },
   },
