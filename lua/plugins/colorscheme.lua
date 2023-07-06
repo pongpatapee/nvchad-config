910ba8ac03fd35788c7fae682669e54add890ef1
pongpatapee <pongpatapee1021@gmail.com>
2023-07-05 22:50:32 -0700

    changed default colorscheme to onedark

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -16,7 +16,7 @@ return {
     lazy = false,
     opts = {
       --main options
-      style = "warmer", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+      style = "dark", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
       transparent = false, -- Show/hide background
       term_colors = true, -- Change terminal color as per the selected theme style
       ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
@@ -51,7 +51,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "onedark",
     },
   },
 }
