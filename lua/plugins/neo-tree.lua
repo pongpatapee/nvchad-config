ac716bd3c669db00df372596ae0906b1cc8eec59
pongpatapee <pongpatapee1021@gmail.com>
2024-02-18 11:37:44 -0500

    create shortcut to delete files to trash in neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -3,6 +3,23 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
       filesystem = {
+        -- commands = {
+        --   delete = function(state)
+        --     local path = state.tree:get_node().path
+        --     vim.fn.system("trash " .. vim.fn.fnameescape(path))
+        --     require("neo-tree.sources.manager").refresh(state.name)
+        --   end,
+        -- },
+        window = {
+          mappings = {
+            ["D"] = function(state)
+              local node = state.tree:get_node()
+              local path = node:get_id()
+              vim.fn.system("trash " .. vim.fn.fnameescape(path))
+            end,
+          },
+        },
+
         filtered_items = {
           visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
           hide_dotfiles = true,
