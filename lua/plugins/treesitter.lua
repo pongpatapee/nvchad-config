898115bb0d5c1e1e342754cdaf120123411a6daa
pongpatapee <pongpatapee1021@gmail.com>
2024-06-18 08:15:54 -0400

    feat: rainbow paren query made html highlighting weird

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -48,7 +48,7 @@ return {
         -- extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
         -- max_file_lines = nil, -- Do not enable for files with more than n lines, int
 
-        query = { "rainbow-parens" },
+        -- query = { "rainbow-parens" },
 
         -- hlgroups = {
         --   "TSRainbowRed",
