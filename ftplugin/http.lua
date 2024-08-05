de38ed96a2746bdd626e1107677046a1bab946bc
pongpatapee <pongpatapee1021@gmail.com>
2024-08-05 18:58:43 -0400

    added keybinds for kulala.nvim

--- ftplugin/http.lua
+++ ftplugin/http.lua
@@ -1,8 +1,30 @@
-vim.api.nvim_set_keymap("n", "<C-p>", ":lua require('kulala').jump_prev()<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<C-n>", ":lua require('kulala').jump_next()<CR>", { noremap = true })
-vim.api.nvim_set_keymap(
+vim.keymap.set("n", "<C-p>", ":lua require('kulala').jump_prev()<CR>", { noremap = true })
+vim.keymap.set("n", "<C-n>", ":lua require('kulala').jump_next()<CR>", { noremap = true })
+vim.keymap.set(
   "n",
-  "<leader>r",
+  "<leader>rr",
   ":lua require('kulala').run()<CR>",
-  { noremap = true, silent = true, desc = "Run Kulala REST call" }
+  { noremap = true, silent = true, desc = "Kulala Rest Run" }
 )
+vim.keymap.set(
+  "n",
+  "<leader>rq",
+  ":lua require('kulala').close()<CR>",
+  { noremap = true, silent = true, desc = "Kulala Rest Quit" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>ry",
+  ":lua require('kulala').copy()<CR>",
+  { noremap = true, silent = true, desc = "Kulala Rest Copy" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>rt",
+  ":lua require('kulala').toggle_view()<CR>",
+  { noremap = true, silent = true, desc = "Kulala Rest Toggle View" }
+)
+
+-- Comment.nvim for .http filetype
+local ft = require("Comment.ft")
+ft.set("http", "#%s")
