7f566bd627a9d5e5c768e46d4c701f6953efd8ca
pongpatapee <pongpatapee1021@gmail.com>
2024-08-23 19:33:09 -0400

    leetcode nvim and changed onedark color

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -29,8 +29,8 @@ return {
     lazy = false,
     opts = {
       --main options
-      style = "warmer", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-      transparent = true, -- Show/hide background
+      style = "darker", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+      transparent = false, -- Show/hide background
       term_colors = true, -- Change terminal color as per the selected theme style
       ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
       cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
