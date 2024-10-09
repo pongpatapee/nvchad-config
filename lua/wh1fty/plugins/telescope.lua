9908e4557a417190fcad9e67f814a666f0277e6b
pongpatapee <pongpatapee1021@gmail.com>
2024-10-09 02:28:50 -0400

    Neovim options

--- lua/wh1fty/plugins/telescope.lua
+++ lua/wh1fty/plugins/telescope.lua
@@ -7,8 +7,7 @@ return {
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+    vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = "[S]earch Help Tags"})
   end
-
 }
 
