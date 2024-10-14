0c627eaee8a7f4ec6b041f7383225b3b5fa12455
pongpatapee <pongpatapee1021@gmail.com>
2024-10-13 20:33:12 -0400

    init conform formatter config

---
+++ lua/wh1fty/plugins/lsp/conform.lua
@@ -0,0 +1,71 @@
+return {
+    "stevearc/conform.nvim",
+    cmd = { "ConformInfo" },
+    opts = {
+        notify_on_error = false,
+        -- format_on_save = function(bufnr)
+        --     -- Disable "format_on_save lsp_fallback" for languages that don't
+        --     -- have a well standardized coding style. You can add additional
+        --     -- languages here or re-enable it for the disabled ones.
+        --     local disable_filetypes = { c = true, cpp = true }
+        --     local lsp_format_opt
+        --     if disable_filetypes[vim.bo[bufnr].filetype] then
+        --         lsp_format_opt = "never"
+        --     else
+        --         lsp_format_opt = "fallback"
+        --     end
+        --     return {
+        --         timeout_ms = 500,
+        --         lsp_format = lsp_format_opt,
+        --     }
+        -- end,
+
+        formatters_by_ft = {
+            lua = { "stylua" },
+            -- Conform will run multiple formatters sequentially
+            -- python = { "isort", "black" },
+            -- You can customize some of the format options for the filetype (:help conform.format)
+            -- Conform will run the first available formatter
+            -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        },
+    },
+    keys = {
+        {
+            "<leader>cf",
+            function()
+                require("conform").format({ async = true, lsp_format = "fallback" })
+            end,
+            mode = "n",
+            desc = "Code Format buffer",
+        },
+        {
+            "<leader>ctf",
+            function()
+                vim.g.auto_format_on_save = not vim.g.auto_format_on_save
+                print("Auto format: " .. vim.g.auto_format_on_save)
+            end,
+            mode = "n",
+            desc = "Code Toggle Auto Format",
+        },
+    },
+
+    config = function(_, opts)
+        require("conform").setup(opts)
+        -- Setup auto save myself so I can toggle it on and off
+
+        -- global var for toggling auto format
+        vim.g.auto_format_on_save = true
+
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            pattern = "*",
+            callback = function(args)
+                -- global var for toggling auto format
+                if not vim.g.auto_format_on_save then
+                    return
+                end
+
+                require("conform").format({ bufnr = args.buf, lsp_format = "fallback", quiet = true })
+            end,
+        })
+    end,
+}
--- lua/wh1fty/plugins/lsp/lsp.lua
+++ lua/wh1fty/plugins/lsp/lsp.lua
@@ -18,14 +18,14 @@ return {
 
         lspconfig.lua_ls.setup({
             diagnostics = {
-                globals = { 'vim' }
-            }
+                globals = { "vim" },
+            },
         })
 
         -- Add lsp keybinds
         vim.api.nvim_create_autocmd("LspAttach", {
             callback = function(e)
-                local builtin = require('telescope.builtin')
+                local builtin = require("telescope.builtin")
                 -- vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = e.buf, desc = "Goto Definition" })
                 -- vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = e.buf, desc = "References" })
                 -- vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = e.buf, desc = "Goto Implementation" })
@@ -33,18 +33,51 @@ return {
                 vim.keymap.set("n", "gr", builtin.lsp_references, { buffer = e.buf, desc = "References" })
                 vim.keymap.set("n", "gi", builtin.lsp_implementations, { buffer = e.buf, desc = "Goto Implementation" })
                 vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = e.buf, desc = "Goto Declaration" })
-                vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, { buffer = e.buf, desc = "Goto Type Definition" })
+                vim.keymap.set(
+                    "n",
+                    "gT",
+                    vim.lsp.buf.type_definition,
+                    { buffer = e.buf, desc = "Goto Type Definition" }
+                )
                 vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = e.buf, desc = "Hover" })
                 vim.keymap.set("n", "gK", vim.lsp.buf.signature_help, { buffer = e.buf, desc = "Signature Help" })
                 vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { buffer = e.buf, desc = "Code Rename" })
                 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = e.buf, desc = "Code Action" })
-                vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { buffer = e.buf, desc = "Code Format" })
-                vim.keymap.set("n", "<leader>cd", vim.diagnostic.open_float, { buffer = e.buf, desc = "Code Diagnostics" })
+                -- vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { buffer = e.buf, desc = "Code Format" })
+                vim.keymap.set(
+                    "n",
+                    "<leader>cd",
+                    vim.diagnostic.open_float,
+                    { buffer = e.buf, desc = "Code Diagnostics" }
+                )
                 vim.keymap.set("n", "[d", vim.diagnostic.goto_next, { buffer = e.buf, desc = "Next Diagnostics" })
                 vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, { buffer = e.buf, desc = "Prev Diagnostics" })
-                vim.keymap.set("n", "<leader>ss", builtin.lsp_document_symbols, { buffer = e.buf, desc = "Search Document Symbols" })
-                vim.keymap.set("n", "<leader>sS", builtin.lsp_workspace_symbols, { buffer = e.buf, desc = "Search Workspace Symbols" })
-            end
+                vim.keymap.set(
+                    "n",
+                    "<leader>ss",
+                    builtin.lsp_document_symbols,
+                    { buffer = e.buf, desc = "Search Document Symbols" }
+                )
+                vim.keymap.set(
+                    "n",
+                    "<leader>sS",
+                    builtin.lsp_workspace_symbols,
+                    { buffer = e.buf, desc = "Search Workspace Symbols" }
+                )
+            end,
         })
+
+        -- local capabilities = vim.lsp.protocol.make_client_capabilities()
+        -- capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+
+        -- require("mason-lspconfig").setup({
+        --     handlers = {
+        --         function(server_name)
+        --             require("lspconfig")[server_name].setup({
+        --                 capabilities = capabilities,
+        --             })
+        --         end,
+        --     },
+        -- })
     end,
 }
