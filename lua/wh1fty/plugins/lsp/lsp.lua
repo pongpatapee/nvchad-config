676c0e6f8272b9cadb7b9056d0a4a357b7c6a48e
pongpatapee <pongpatapee1021@gmail.com>
2024-10-13 23:09:22 -0400

    properly configured lsp/mason/cmp with server capabilities

--- lua/wh1fty/plugins/lsp/lsp.lua
+++ lua/wh1fty/plugins/lsp/lsp.lua
@@ -9,16 +9,46 @@ return {
     },
 
     config = function()
+        local servers = {
+            lua_ls = {
+                -- cmd = {...},
+                -- filetypes = { ...},
+                -- capabilities = {},
+                settings = {
+                    Lua = {
+                        completion = {
+                            callSnippet = "Replace",
+                        },
+                        -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+                        -- diagnostics = { disable = { 'missing-fields' } },
+                    },
+                },
+                -- diagnostics = {
+                --     globals = { "vim" },
+                -- },
+            },
+        }
+        local ensure_installed = { "lua_ls" }
+
+        -- LSP servers and clients are able to communicate to each other what features they support.
+        --  By default, Neovim doesn't support everything that is in the LSP specification.
+        --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
+        --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+
         require("mason").setup()
         require("mason-lspconfig").setup({
-            ensure_installed = { "lua_ls" },
-        })
-
-        local lspconfig = require("lspconfig")
-
-        lspconfig.lua_ls.setup({
-            diagnostics = {
-                globals = { "vim" },
+            ensure_installed = ensure_installed,
+            handlers = {
+                function(server_name)
+                    local server = servers[server_name] or {}
+                    -- This handles overriding only values explicitly passed
+                    -- by the server configuration above. Useful when disabling
+                    -- certain features of an LSP (for example, turning off formatting for ts_ls)
+                    server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+                    require("lspconfig")[server_name].setup(server)
+                end,
             },
         })
 
@@ -66,18 +96,5 @@ return {
                 )
             end,
         })
-
-        -- local capabilities = vim.lsp.protocol.make_client_capabilities()
-        -- capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
-
-        -- require("mason-lspconfig").setup({
-        --     handlers = {
-        --         function(server_name)
-        --             require("lspconfig")[server_name].setup({
-        --                 capabilities = capabilities,
-        --             })
-        --         end,
-        --     },
-        -- })
     end,
 }
--- lua/wh1fty/plugins/lsp/nvim-cmp.lua
+++ lua/wh1fty/plugins/lsp/nvim-cmp.lua
@@ -13,16 +13,15 @@ return {
     },
 
     config = function()
-
         -- -- For friendly-snippets
         -- require('luasnip.loaders.from_vscode').lazy_load()
 
-        local cmp = require 'cmp'
+        local cmp = require("cmp")
 
         cmp.setup({
             snippet = {
                 expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                    require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
                 end,
             },
 
@@ -34,20 +33,20 @@ return {
             },
 
             mapping = cmp.mapping.preset.insert({
-                ['<C-n>'] = cmp.mapping.select_next_item(),
-                ['<C-p>'] = cmp.mapping.select_prev_item(),
-                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-                ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                ['<C-Space>'] = cmp.mapping.complete(),
-                ['<C-e>'] = cmp.mapping.abort(),
-                ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                ["<C-n>"] = cmp.mapping.select_next_item(),
+                ["<C-p>"] = cmp.mapping.select_prev_item(),
+                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<C-e>"] = cmp.mapping.abort(),
+                ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
             }),
             sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' }, -- For luasnip users.
+                { name = "nvim_lsp" },
+                { name = "luasnip" }, -- For luasnip users.
             }, {
-                { name = 'buffer' },
-            })
+                { name = "buffer" },
+            }),
         })
 
         -- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
@@ -59,35 +58,26 @@ return {
                 { name = 'buffer' },
             })
         })
-        require("cmp_git").setup() ]] --
+        require("cmp_git").setup() ]]
+        --
 
         -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-        cmp.setup.cmdline({ '/', '?' }, {
+        cmp.setup.cmdline({ "/", "?" }, {
             mapping = cmp.mapping.preset.cmdline(),
             sources = {
-                { name = 'buffer' }
-            }
+                { name = "buffer" },
+            },
         })
 
         -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-        cmp.setup.cmdline(':', {
+        cmp.setup.cmdline(":", {
             mapping = cmp.mapping.preset.cmdline(),
             sources = cmp.config.sources({
-                { name = 'path' }
+                { name = "path" },
             }, {
-                { name = 'cmdline' }
+                { name = "cmdline" },
             }),
-            matching = { disallow_symbol_nonprefix_matching = false }
+            matching = { disallow_symbol_nonprefix_matching = false },
         })
-
-        -- TODO: setup cmp lsp capabilities
-        -- -- Set up lspconfig.
-        -- local capabilities = require('cmp_nvim_lsp').default_capabilities()
-        -- -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-        -- require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
-        --     capabilities = capabilities
-        -- }
-    end
-
-
+    end,
 }
