401638f7409ea380f3ebe7dc12c9d88684adfec4
pongpatapee <pongpatapee1021@gmail.com>
2024-10-18 19:39:49 -0400

    feat: oil now shows current dir

--- lua/wh1fty/plugins/editor/oil.lua
+++ lua/wh1fty/plugins/editor/oil.lua
@@ -5,6 +5,16 @@ return {
     -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
+        function _G.get_oil_winbar()
+            local dir = require("oil").get_current_dir()
+            if dir then
+                return vim.fn.fnamemodify(dir, ":~")
+            else
+                -- If there is no current dir (e.g. over ssh), just show buf name
+                return vim.api.nvim_buf_get_name(0)
+            end
+        end
+
         require("oil").setup({
             columns = {
                 "icon",
@@ -23,6 +33,10 @@ return {
             delete_to_trash = true,
             view_options = {
                 show_hidden = true,
+            },
+
+            win_options = {
+                winbar = "%{v:lua.get_oil_winbar()}",
             },
         })
 
