5196866268468a789f82a16e785dd47b004d3197
pongpatapee <pongpatapee1021@gmail.com>
2024-10-19 16:43:07 -0400

    feat: added git diffview plugin

---
+++ lua/wh1fty/plugins/editor/diffview.lua
@@ -0,0 +1,22 @@
+return {
+    "sindrets/diffview.nvim",
+    opts = {},
+    keys = {
+        {
+            "<leader>gdo",
+            "<cmd>DiffviewOpen<cr>",
+            desc = "[G]it [D]iffView[O]pen",
+        },
+        {
+
+            "<leader>gdc",
+            "<cmd>DiffviewClose<cr>",
+            desc = "[G]it [D]iffView[C]lose",
+        },
+        {
+            "<leader>gdh",
+            "<cmd>DiffviewFileHistory<cr>",
+            desc = "[G]it [D]iffViewFile[H]istory",
+        },
+    },
+}
