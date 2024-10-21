f8d4e9f355961feeee41fc1e2d74db64fead2d84
pongpatapee <pongpatapee1021@gmail.com>
2024-10-21 19:52:26 -0400

    feat(dreassing): added pluggin

---
+++ lua/wh1fty/plugins/editor/dressing.lua
@@ -0,0 +1,7 @@
+return {
+    -- adds extra ui from new neovim core
+    -- via vim.ui.select or vim.ui.input
+    -- e.g., lsp rename or selecting with telescope
+    "stevearc/dressing.nvim",
+    opts = {},
+}
