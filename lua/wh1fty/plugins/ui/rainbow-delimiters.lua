cb850e90e2bb43351e711ffe831bec3937839ed7
pongpatapee <pongpatapee1021@gmail.com>
2024-11-05 23:47:03 -0500

    feat(rainbow): changed paren color and todo for custom color

--- lua/wh1fty/plugins/ui/rainbow-delimiters.lua
+++ lua/wh1fty/plugins/ui/rainbow-delimiters.lua
@@ -17,11 +17,21 @@ return {
 
             -- vscode like highlight groups
             "RainbowDelimiterYellow",
-            "RainbowDelimiterViolet",
+            -- "RainbowDelimiterViolet",
+            "RainbowDelimiterRed",
             "RainbowDelimiterBlue",
         },
     },
     config = function(_, opts)
+        -- TODO: Get custom highlight working
+
+        -- RainbowDelimiterRed xxx guifg=#c586c0
+        -- RainbowDelimiterOrange xxx guifg=#ce9178
+        -- RainbowDelimiterYellow xxx guifg=#d7ba7d
+        -- RainbowDelimiterGreen xxx guifg=#6a9955
+        -- RainbowDelimiterCyan xxx guifg=#4ec9b0
+        -- RainbowDelimiterBlue xxx guifg=#18a2fe
+        -- RainbowDelimiterViolet xxx guifg=#646695
         require("rainbow-delimiters.setup").setup(opts)
     end,
 }
