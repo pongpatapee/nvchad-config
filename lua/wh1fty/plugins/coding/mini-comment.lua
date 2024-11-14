e0a91555ba77914c04d70274aeb2193ef05e9cd7
pongpatapee <pongpatapee1021@gmail.com>
2024-11-14 16:51:35 -0500

    feat: comment keybind in insert mode

--- lua/wh1fty/plugins/coding/mini-comment.lua
+++ lua/wh1fty/plugins/coding/mini-comment.lua
@@ -26,4 +26,18 @@ return {
             textobject = "gc",
         },
     },
+    keys = {
+        -- Use same keybind in insert mode
+        {
+            mode = "i",
+            "<C-_>",
+            function()
+                local curr_win = vim.api.nvim_get_current_win()
+                local cursor_pos = vim.api.nvim_win_get_cursor(curr_win)
+                local line = cursor_pos[1]
+
+                require("mini.comment").toggle_lines(line, line)
+            end,
+        },
+    },
 }
